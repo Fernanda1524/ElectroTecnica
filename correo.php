20a6dcbf0e7ab53d2540cf9bf9c6dc38f9cb719c
Fernanda Gonzalez <fernanda.gtp87@gmail.com>
2021-01-04 22:01:52 -0300

    correo.php

--- correo.php
+++ correo.php
@@ -6,7 +6,7 @@
  */
 
 //Import the PHPMailer class into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/PHPMailer.php';
